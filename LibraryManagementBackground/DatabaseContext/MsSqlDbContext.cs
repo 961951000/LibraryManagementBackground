28346c8eada26945ff7f36379715ba07d79f4d98
cxw6754@zzdd.com.cn <c15000058373>
2017-04-11 15:39:12 +0800

    update

---
+++ LibraryManagementBackground/DatabaseContext/MsSqlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementBackground.DatabaseContext
+{
+    public class MsSqlDbContext : DbContext
+    {
+        public MsSqlDbContext() : base("name=MsSqlDbContext") { }
+    }
+}
